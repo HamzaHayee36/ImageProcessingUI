edb1e8e74e2227ea4b74376aa94e54ce331cda11
Hamza <110095038+HamzaHayee36@users.noreply.github.com>
2023-11-28 20:29:16 +0500

    new commit

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-document.getElementById('imageUpload').addEventListener('change', function(event) {
-  const previewContainer = document.getElementById('previewContainer');
-  const file = event.target.files[0];
-  if (file) {
-      const reader = new FileReader();
-      reader.onload = function(e) {
-          const img = document.createElement('img');
-          img.src = e.target.result;
-          img.style.maxWidth = '100%';
-          img.style.height = 'auto';
-          previewContainer.innerHTML = '';
-          previewContainer.appendChild(img);
-      };
-      reader.readAsDataURL(file);
+document.getElementById('image-upload').addEventListener('change', function(event) {
+  const inputImage = document.getElementById('input-image');
+  const outputImage = document.getElementById('output-image');
+
+  // Display the uploaded image in the input section
+  const reader = new FileReader();
+  reader.onload = function(e) {
+    inputImage.src = e.target.result;
+    // For demonstration, we'll just copy the input image to the output
+    outputImage.src = e.target.result;
   }
+  reader.readAsDataURL(event.target.files[0]);
 });
 
-document.getElementById('resetButton').addEventListener('click', function() {
-  const previewContainer = document.getElementById('previewContainer');
-  previewContainer.innerHTML = 'Image preview will be shown here...';
-  document.getElementById('imageUpload').value = '';
+document.getElementById('download-btn').addEventListener('click', function() {
+  const outputImage = document.getElementById('output-image');
+  const link = document.createElement('a');
+  link.href = outputImage.src;
+  link.download = 'processed-image.png';
+  link.click();
 });
 
-document.getElementById('runButton').addEventListener('click', function() {
-  // Add logic for processing the image
-  alert('Run button clicked. Implement the processing logic.');
-});
+// Add event listeners and functions for other buttons as needed
