2d298f278ae7ec3f55d918a0c11800048671710d
Hamza <110095038+HamzaHayee36@users.noreply.github.com>
2023-11-29 11:36:35 +0500

    new commit

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 document.getElementById('image-upload').addEventListener('change', function(event) {
   const inputImage = document.getElementById('input-image');
-  const outputImage = document.getElementById('output-image');
 
-  // Display the uploaded image in the input section
-  const reader = new FileReader();
-  reader.onload = function(e) {
-    inputImage.src = e.target.result;
-    // For demonstration, we'll just copy the input image to the output
-    outputImage.src = e.target.result;
+  // Check if a file was selected
+  if (event.target.files && event.target.files[0]) {
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+      // Set the source of the input image and display it
+      inputImage.src = e.target.result;
+      inputImage.style.display = 'block';
+    }
+
+    // Read the file as a Data URL
+    reader.readAsDataURL(event.target.files[0]);
   }
-  reader.readAsDataURL(event.target.files[0]);
 });
 
 document.getElementById('download-btn').addEventListener('click', function() {
   const outputImage = document.getElementById('output-image');
-  const link = document.createElement('a');
-  link.href = outputImage.src;
-  link.download = 'processed-image.png';
-  link.click();
+  if(outputImage.src) {
+    // Create a link and set the URL to the image source
+    const link = document.createElement('a');
+    link.href = outputImage.src;
+    // Set the download attribute to a default file name
+    link.download = 'processed-image.png';
+    // Trigger the download
+    link.click();
+  } else {
+    alert("No image to download.");
+  }
 });
 
-// Add event listeners and functions for other buttons as needed
+// You can add additional event listeners and functions for other buttons as needed
