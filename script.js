6face305c5d7b429f36d984c82cc68f57cbd795b
Hamza <110095038+HamzaHayee36@users.noreply.github.com>
2023-11-29 15:01:43 +0500

    made changes in uploading methods 4

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 document.getElementById('image-upload').addEventListener('change', function(event) {
   const inputImage = document.getElementById('input-preview-image');
+  const fileNameDisplay = document.getElementById('file-name');
+  const uploadLabel = document.querySelector('.upload-label');
 
   if (event.target.files && event.target.files[0]) {
+    const file = event.target.files[0];
     const reader = new FileReader();
 
     reader.onload = function(e) {
       inputImage.src = e.target.result;
       inputImage.style.display = 'block';
+      fileNameDisplay.textContent = file.name; // Display the file name
+      uploadLabel.style.display = 'none'; // Hide the upload label
     };
 
-    reader.readAsDataURL(event.target.files[0]);
+    reader.readAsDataURL(file);
   }
 });
 
