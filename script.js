adb26a81b7fa038949c436008a08352177bfb560
Hamza <110095038+HamzaHayee36@users.noreply.github.com>
2023-11-29 14:51:45 +0500

    made changes in uploading methods 3

--- script.js
+++ script.js
@@ -1,34 +1,26 @@
 document.getElementById('image-upload').addEventListener('change', function(event) {
-  const inputImage = document.getElementById('input-image');
+  const inputImage = document.getElementById('input-preview-image');
 
-  // Check if a file was selected
   if (event.target.files && event.target.files[0]) {
     const reader = new FileReader();
 
     reader.onload = function(e) {
-      // Set the source of the input image and display it
       inputImage.src = e.target.result;
       inputImage.style.display = 'block';
-    }
+    };
 
-    // Read the file as a Data URL
     reader.readAsDataURL(event.target.files[0]);
   }
 });
 
 document.getElementById('download-btn').addEventListener('click', function() {
-  const outputImage = document.getElementById('output-image');
-  if(outputImage.src) {
-    // Create a link and set the URL to the image source
+  const outputImage = document.getElementById('output-preview-image');
+  if (outputImage.src) {
     const link = document.createElement('a');
     link.href = outputImage.src;
-    // Set the download attribute to a default file name
     link.download = 'processed-image.png';
-    // Trigger the download
     link.click();
   } else {
     alert("No image to download.");
   }
 });
-
-// You can add additional event listeners and functions for other buttons as needed
